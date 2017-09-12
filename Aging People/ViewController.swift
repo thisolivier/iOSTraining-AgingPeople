d464782bba4fcfe8ad9a8e25312d0d9d6f925896
Olivier Butler <olivier.butler@gmail.com>
2017-09-11 21:22:21 -0700

    App now works, 12 names displayed with random ages on right. Could use some font size adjusting.

--- Aging People/ViewController.swift
+++ Aging People/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var masterTable: UITableView!
+    var peopleArr = [
+        "Sunny Dee",
+        "Samantha Dellacroix",
+        "Hussy Charlotte",
+        "Flaming Heroine",
+        "Katy Fearless",
+        "Jocasta Sandbury",
+        "Melissa Enterdown",
+        "Hoozie Junkfort",
+        "Lizzy Larg",
+        "Gretta Hurtful",
+        "Victoria Driller",
+        "Pillbox Queen"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        masterTable.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peopleArr.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let personCell = tableView.dequeueReusableCell(withIdentifier: "Person", for:indexPath)
+        let randomAge = Int(arc4random_uniform(UInt32(90))) + 8
+        personCell.textLabel?.text = peopleArr[indexPath.row]
+        personCell.detailTextLabel?.text = "Has lived \(randomAge) years"
+        return personCell
+    }
+}
